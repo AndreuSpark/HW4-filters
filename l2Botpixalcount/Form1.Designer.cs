34ab6b0ddcc14f00412a97f00ac4cf239711e768
AndreuSpark <114961548+AndreuSpark@users.noreply.github.com>
2023-02-20 15:36:04 -0500

    HW4

---
+++ l2Botpixalcount/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿
+namespace l2Botpixalcount
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.Hugemin = new System.Windows.Forms.TrackBar();
+            this.Hugemax = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hugemin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hugemax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(209, 202);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(239, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(209, 202);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(484, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(209, 202);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(734, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(209, 202);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(981, 0);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(209, 202);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(0, 250);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(209, 202);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // Hugemin
+            // 
+            this.Hugemin.Location = new System.Drawing.Point(239, 270);
+            this.Hugemin.Maximum = 250;
+            this.Hugemin.Name = "Hugemin";
+            this.Hugemin.Size = new System.Drawing.Size(209, 56);
+            this.Hugemin.TabIndex = 6;
+            this.Hugemin.Scroll += new System.EventHandler(this.Hugemin_Scroll);
+            // 
+            // Hugemax
+            // 
+            this.Hugemax.Location = new System.Drawing.Point(239, 208);
+            this.Hugemax.Maximum = 250;
+            this.Hugemax.Name = "Hugemax";
+            this.Hugemax.Size = new System.Drawing.Size(209, 56);
+            this.Hugemax.TabIndex = 7;
+            this.Hugemax.Scroll += new System.EventHandler(this.Hugemax_Scroll);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(495, 270);
+            this.trackBar1.Maximum = 250;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(209, 56);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(495, 208);
+            this.trackBar2.Maximum = 250;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(209, 56);
+            this.trackBar2.TabIndex = 9;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(734, 270);
+            this.trackBar3.Maximum = 250;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(209, 56);
+            this.trackBar3.TabIndex = 10;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(734, 208);
+            this.trackBar4.Maximum = 250;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(209, 56);
+            this.trackBar4.TabIndex = 11;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(981, 230);
+            this.trackBar5.Maximum = 250;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(209, 56);
+            this.trackBar5.TabIndex = 12;
+            this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "sorce";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(236, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "hue";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(481, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Saterashion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(731, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "value";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(978, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 17);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Biniry";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(-3, 250);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 17);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "combined";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1444, 679);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.Hugemax);
+            this.Controls.Add(this.Hugemin);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.form1_closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hugemin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hugemax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.TrackBar Hugemin;
+        private System.Windows.Forms.TrackBar Hugemax;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ l2Botpixalcount/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using System.Threading;
+using Emgu.CV.Structure;
+
+namespace l2Botpixalcount
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private Thread thread;
+        private int HugeThreshholdMax = 00;
+        private int HugeThreshholdMin = 0;
+        private int HugeThreshholdSatMax = 0;
+        private int HugeThreshholdSatMin = 0;
+        private int HugeThreshholdValMax = 0;
+        private int HugeThreshholdValMin = 0;
+        private int  BiniryThreshhold=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _capture = new VideoCapture(1);
+            thread = new Thread(DisplayWebcam);
+            thread.Start();
+            Hugemax.Value = HugeThreshholdMax;
+            Hugemin.Value = HugeThreshholdMin;
+            trackBar1.Value = HugeThreshholdSatMax;
+            trackBar2.Value = HugeThreshholdSatMin;
+            trackBar3.Value = HugeThreshholdValMax;
+            trackBar4.Value = HugeThreshholdValMin;
+            trackBar5.Value = BiniryThreshhold;
+        }
+        private void DisplayWebcam()
+        {
+            while (_capture.IsOpened)
+            {//frame maintinens
+
+                Mat frame = _capture.QueryFrame(); //gets frame
+                Mat resizedFrame = Resize(frame);//Resize frame
+                pictureBox1.Image = resizedFrame.ToBitmap();//displays frame
+
+                Mat[] hsvChannels = ConvetToHVS(frame);//converts frame to hvs
+                Mat Hue=displayHue(hsvChannels);//displays hue
+                Mat Sat = displaySaterashion(hsvChannels);
+                Mat Val =displayValue(hsvChannels);
+                displaybinery(frame);
+                displayCombined(Hue, Sat, Val);
+
+
+            }
+
+        }
+        private void displaybinery(Mat frame)
+        {
+            Image<Bgr, Byte> imeg = frame.ToImage<Bgr, byte>();
+            Image<Gray, Byte> imgGray = imeg.Convert<Gray, byte>().ThresholdBinary(new Gray( BiniryThreshhold), new Gray(255));
+            Mat newframe = imgGray.Mat;
+            pictureBox5.Image = Resize(newframe).ToBitmap();
+        }
+        
+        private void displayCombined(Mat hue ,Mat sat ,Mat val) 
+        {
+            Mat combinedImige = new Mat();
+            CvInvoke.BitwiseAnd(hue, sat, combinedImige);
+            CvInvoke.BitwiseAnd(combinedImige, val, combinedImige);
+            Mat filterdcombinedImige = CvInvoke.GetStructuringElement(Emgu.CV.CvEnum.ElementShape.Cross, new Size(4, 4), new Point(1, 1));
+            CvInvoke.Dilate(combinedImige, combinedImige, filterdcombinedImige, new Point(1, 1), 1, Emgu.CV.CvEnum.BorderType.Default, new MCvScalar());
+            CvInvoke.Erode(combinedImige, combinedImige, filterdcombinedImige, new Point(1, 1), 1, Emgu.CV.CvEnum.BorderType.Default, new MCvScalar());
+            Invoke(new Action(() => { pictureBox6.Image = Resize(combinedImige).ToBitmap(); }));
+        }
+        private Mat displayHue(Mat[] hsvChannel)
+        {
+            Mat hueFillter = new Mat();
+            CvInvoke.InRange(hsvChannel[0], new ScalarArray(HugeThreshholdMin),new ScalarArray(HugeThreshholdMax),hueFillter);//sets thresholdse
+            Mat resizedhueFillter = Resize(hueFillter);//resizes frame and stors as new mat
+            Invoke(new Action(() => { pictureBox2.Image = resizedhueFillter.ToBitmap();}));//turns to bitmap 
+            return hueFillter;
+        }
+        private Mat displaySaterashion(Mat[] hsvChannel)
+        {
+            Mat SatFillter = new Mat();
+            CvInvoke.InRange(hsvChannel[1], new ScalarArray(HugeThreshholdSatMin), new ScalarArray(HugeThreshholdSatMax), SatFillter);//sets thresholdse
+            Mat resizedhueFillter = Resize(SatFillter);//resizes frame and stors as new mat
+            Invoke(new Action(() => { pictureBox3.Image = resizedhueFillter.ToBitmap(); }));//turns to bitmap 
+            return SatFillter;
+        }
+        private Mat displayValue(Mat[] hsvChannel)
+        {
+            Mat SatFillter = new Mat();
+            CvInvoke.InRange(hsvChannel[2], new ScalarArray(HugeThreshholdValMin), new ScalarArray(HugeThreshholdValMax), SatFillter);//sets thresholdse
+            Mat resizedhueFillter = Resize(SatFillter);//resizes frame and stors as new mat
+            Invoke(new Action(() => { pictureBox4.Image = resizedhueFillter.ToBitmap(); }));//turns to bitmap 
+            return SatFillter;
+        }
+        private Mat[] ConvetToHVS(Mat framein)
+        {
+            // convert the image to an HVS image:
+            Mat hvsframe = new Mat();
+            CvInvoke.CvtColor(framein, hvsframe, Emgu.CV.CvEnum.ColorConversion.Bgr2Hsv);
+
+            Mat[] hsvChannels = hvsframe.Split();
+            return hsvChannels;
+
+        }
+        private Mat Resize(Mat framein)
+        {
+            Mat frame = framein.Clone();
+            int newHight = (frame.Size.Height * pictureBox1.Size.Width) / frame.Size.Width;
+            Size newSize = new Size(pictureBox1.Size.Width, newHight);
+            CvInvoke.Resize(frame, frame, newSize);
+            return frame;
+        }
+        private void form1_closing(object sender, FormClosingEventArgs e)
+        {
+            thread.Abort();
+        }
+      
+        private void Hugemax_Scroll(object sender, EventArgs e)
+        {
+            HugeThreshholdMax = Hugemax.Value;
+        }
+
+        private void Hugemin_Scroll(object sender, EventArgs e)
+        {
+            HugeThreshholdMin = Hugemin.Value;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            HugeThreshholdSatMin = trackBar2.Value;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            HugeThreshholdSatMax = trackBar1.Value;
+        }
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            HugeThreshholdValMax = trackBar4.Value;
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            HugeThreshholdValMin = trackBar3.Value;
+        }
+
+        private void trackBar5_Scroll(object sender, EventArgs e)
+        {
+            BiniryThreshhold = trackBar5.Value;
+        }
+    }
+}
